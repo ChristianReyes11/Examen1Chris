83eacc3e9017196b50c0e0507e91608d3b83e628
ChristianReyes11 <christianamaro2000@gmail.com>
2023-06-12 11:29:33 -0500

    Inicializacion

---
+++ src/main/java/org/uv/Examen1ChristianEduardoAmaroReyes/Examen1ChristianEduardoAmaroReyesApplication.java
@@ -0,0 +1,13 @@
+package org.uv.Examen1ChristianEduardoAmaroReyes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Examen1ChristianEduardoAmaroReyesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Examen1ChristianEduardoAmaroReyesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/uv/Examen1ChristianEduardoAmaroReyes/Examen1ChristianEduardoAmaroReyesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.uv.Examen1ChristianEduardoAmaroReyes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Examen1ChristianEduardoAmaroReyesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
